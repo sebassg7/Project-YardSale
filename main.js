46d26ee0862986270ce977641fb38ee27759ee23
sebassg7 <sebastlopez@unicauca.edu.co>
2023-03-09 21:03:53 -0500

    Menu de celular agregado

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const textEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
+const menuBurgerIcon = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
 textEmail.addEventListener('click', toggleDesktopMenu);
+menuBurgerIcon.addEventListener('click',toggleMenuMobile);
 
 function toggleDesktopMenu(){
     menuDesktop.classList.toggle('inactive');
+}
+
+function toggleMenuMobile(){
+    menuMobile.classList.toggle('oculto');
 }
